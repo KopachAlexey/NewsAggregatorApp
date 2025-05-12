94feeef0f92e8293c88dad72c5243308fc6079a0
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:56:34 +0300

    добавлен путь к компоненту редактирования пользователя

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { NewsDetailsComponent } from './components/news-details/news-details.com
 import { LoginComponent } from './components/login/login.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { AppSettingsComponent } from './components/app-settings/app-settings.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/news-feed?pageNumber=1', pathMatch: 'full'},
@@ -12,6 +13,7 @@ export const routes: Routes = [
   {path: 'news/:id', component: NewsDetailsComponent },
   {path: 'login', component: LoginComponent },
   {path: 'new-user', component: AddUserComponent},
+  {path: 'edit-user', component: EditUserComponent},
   {path: 'app-settings', component: AppSettingsComponent},
   {path: '**', component: NotFoundComponent }
 ];
