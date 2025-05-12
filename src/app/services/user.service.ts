55d78c64d8e542194589615b0a77d898c1c188db
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:50:35 +0300

    Теперь данные отпровляются не формой а json в body, исправленны пути к ресурсам

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,24 +4,28 @@ import { catchError, Observable, of, throwError } from 'rxjs';
 import { User } from '../models/user';
 import { AddResourceResponse } from '../models/add-resource-response';
 import { NewsAggregatorService } from './news-aggregator.service';
+import { EditUser } from '../models/edit-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private userResource : string = 'User';
+  private updateUserNewsRareResource : string = 'User/update-user-news-rate';
   private readonly newsAggregator : NewsAggregatorService;
 
   constructor(newsAggregator : NewsAggregatorService) {this.newsAggregator = newsAggregator}
 
   addUser(newUser : NewUser) : Observable<AddResourceResponse> {
-    let newUserFromData = new FormData();
-    newUserFromData.append('password', newUser.password);
-    newUserFromData.append('login', newUser.login);
-    newUserFromData.append('email', newUser.email);
-    return this.newsAggregator.post<AddResourceResponse>(this.userResource, newUserFromData).pipe(
+    const body = {
+      password : newUser.password,
+      login : newUser.login,
+      email : newUser.email
+    };
+    return this.newsAggregator
+    .post<AddResourceResponse>(this.userResource, body).pipe(
       catchError((error) => {
-        return throwError(() => error)
+        return throwError(() => error);
       })
     );
   }
@@ -30,10 +34,26 @@ export class UserService {
     const userResourceById = `${this.userResource}/${id}`;
     return this.newsAggregator.get<User>(userResourceById).pipe(
       catchError((error) => {
-        return throwError(() => error)
+        return throwError(() => error);
       })
     );
   }
 
+  editUserById(id : string, editUser : EditUser) : Observable<void> {
+    const userResourceById = `${this.userResource}/${id}`;
+    return this.newsAggregator.patch<void>(userResourceById, editUser).pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
 
+  updateUserNewsRateById(id : string, newsRate : number) : Observable<void> {
+    const userResourceById = `${this.updateUserNewsRareResource}/${id}`
+    return this.newsAggregator.patch<void>(userResourceById, {newsRate}).pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
 }
