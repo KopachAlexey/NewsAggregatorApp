f1226680889456e4858b50c828d1b3ac9d60f346
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:22:31 +0300

    Полоностью переделан основной компонент

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,18 @@ import { TokenData } from './models/token-data';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { AvatarComponent } from './components/avatar/avatar.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import { NewsAggregatorConsts } from './models/news-aggregator-consts';
+import { UserService } from './services/user.service';
+import { NewsRateService } from './services/news-rate.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule,  AvatarComponent, RouterLink],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule,
+    AvatarComponent, RouterLink, MatSliderModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -25,25 +32,36 @@ export class AppComponent implements OnInit {
   private readonly returnUrlService : ReturnUrlService;
   private readonly authServices : AuthenticationService
   private readonly tokensService : TokensService;
+  private readonly userService : UserService;
+  private readonly newsRateService : NewsRateService;
+  newsRate : number = NewsAggregatorConsts.DefaultNewsRate;
   userData? : TokenData | null;
   title = 'News Aggregator';
   isAuthenticated : boolean = false;
 
-  constructor(authServices : AuthenticationService, returnUrlService : ReturnUrlService, router : Router, tokensService : TokensService) {
+  constructor(authServices : AuthenticationService, returnUrlService : ReturnUrlService,
+    router : Router, tokensService : TokensService, userService : UserService,
+    newsRateService : NewsRateService) {
     this.authServices = authServices;
     this.returnUrlService = returnUrlService;
     this.router = router;
     this.tokensService = tokensService;
+    this.newsRateService = newsRateService;
+    this.userService = userService;
   }
 
   ngOnInit() : void {
     this.tokensService.getAccessTokenData().subscribe( {
       next : (tokenData)=> {
         this.userData = tokenData;
-        if(tokenData)
+        if(tokenData) {
           this.isAuthenticated = true;
-        else
+          this.setUserNewsRate(tokenData.nameid);
+        }
+        else {
           this.isAuthenticated = false;
+          this.newsRateService.setNewsRate(this.newsRate);
+        }
       },
       error : (err) => {
         console.error('error when trying get data from token', err);
@@ -52,20 +70,59 @@ export class AppComponent implements OnInit {
   }
 
   login() : void {
-    this.returnUrlService.setReturnUrl();
+    this.returnUrlService.setReturnUrlAfterLogin();
     this.router.navigate(['/login']);
   }
 
   logout() : void {
-    this.authServices.logout().subscribe({
+    this.authServices.logout()
+    .pipe(
+      finalize( () => {
+         this.isAuthenticated = false;
+         this.tokensService.clearTokens();
+         this.newsRateService.clearNewsRate();
+         this.router.navigate(['/']);
+      })
+    )
+    .subscribe({
       next : () => {
-        this.isAuthenticated = false;
       },
       error : (err) => {
         console.error("logout faild", err);
-      }
+      },
     });
-
   }
 
+  editUser() : void {
+    this.router.navigate(['edit-user']);
+  }
+
+  onPositivityChange() : void {
+    if(!this.userData)
+      return;
+    this.userService.updateUserNewsRateById(this.userData.nameid, this.newsRate).subscribe({
+      next : () => {
+        this.newsRateService.setNewsRate(this.newsRate);
+      },
+      error : (err) => {
+        console.error('error when trying update user news rate', err);
+      }
+    });
+  }
+
+  visibleNewsRate() : number {
+    return this.newsRate * 10 + 50;
+  }
+
+  private setUserNewsRate(userId : string) : void {
+    this.userService.getUserById(userId).subscribe( {
+      next : (user) => {
+        this.newsRateService.setNewsRate(user.newsMinRate);
+        this.newsRate = user.newsMinRate;
+      },
+      error : (err) => {
+        console.error('error when trying get user news rate', err);
+      }
+    });
+  }
 }
