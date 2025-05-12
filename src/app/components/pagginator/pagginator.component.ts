96a8e983cb5134786fac941d22f58e44a62b72d5
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:19:07 +0300

    Добавлен компонент для паггинации по новостным страницам

---
+++ src/app/components/pagginator/pagginator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagginator',
+  imports: [],
+  templateUrl: './pagginator.component.html',
+  styleUrl: './pagginator.component.scss'
+})
+export class PagginatorComponent {
+
+}
