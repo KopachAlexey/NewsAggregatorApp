9156e8f0d1953255b815d1b05da137cb152a2f09
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:54:55 +0300

    Добавленна информация о тестовом рекламном баннере

--- src/app/models/news-aggregator-consts.ts
+++ src/app/models/news-aggregator-consts.ts
@@ -1,3 +1,5 @@
+import { AddCard } from "./add-card";
+
 export class NewsAggregatorConsts {
 
   static DefaultPageSize: number = 10;
@@ -10,6 +12,21 @@ export class NewsAggregatorConsts {
 
   static MinLoginLenth : number = 3;
   static MinPasswordLenth : number = 10;
+
+  static addCards : AddCard[] = [
+    {
+      title : 'Реклама',
+      imgUrl : 'https://thumbs.dreamstime.com/z/advertising-word-cloud-business-concept-58626746.jpg',
+      description : 'Здесь может быть ваша реклама',
+      subtitle : 'По вопросам размещения рекламмы обращайтесь по телефону  +375 (29) 702-83-93',
+      ctaText : 'Перейти',
+      features : [
+        'Долгосрочный эффект',
+        'Ссылки на ваш сайт',
+        'Креативные концепции',
+      ]
+    }
+  ];
 
   private constructor() {}
 }
