d13338c432dbed671784dfa4f351909784c7f0a0
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 23:51:20 +0300

    Добавлен сервис для возврата на предидущий url

--- src/app/services/return-url.service.ts
+++ src/app/services/return-url.service.ts
@@ -13,15 +13,43 @@ export class ReturnUrlService {
 
   clearReurnUrl() : void {
     localStorage.removeItem('returnUrl');
+    localStorage.removeItem('elementId');
   }
 
-  setReturnUrl() : void {
+  clearReurnUrlAfterLogin() : void {
+    localStorage.removeItem('returnAfterLogin');
+  }
+
+  setReturnUrlAfterLogin() : void {
+    localStorage.setItem('returnAfterLogin', this.router.url);
+  }
+
+  returnAfterLogin() : void {
+    let returnUrl = localStorage.getItem('returnAfterLogin');
+    this.clearReurnUrlAfterLogin();
+    this.router.navigateByUrl(returnUrl ?? '');
+  }
+
+  setReturnUrl(elementId? : string) : void {
     localStorage.setItem('returnUrl', this.router.url);
+    if(elementId)
+      localStorage.setItem('elementId', elementId);
   }
 
   redirectToReturnUrl() : void {
     let returnUrl = localStorage.getItem('returnUrl');
+    let elementId = localStorage.getItem('elementId');
     this.clearReurnUrl();
-    this.router.navigateByUrl(returnUrl ?? '');
+    if(elementId) {
+      this.router.navigateByUrl(returnUrl ?? '' , {
+        state : {elementId},
+      });
+    }
+    else {
+      this.router.navigateByUrl(returnUrl ?? '');
+    }
+
   }
+
+
 }
